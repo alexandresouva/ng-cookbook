eb74ff0f9e29320ab3c3442df7d409141a96a351
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2026-07-18 11:51:12 -0300

    chore(#18): remove status factory example

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,9 +1,1 @@
-import { http, HttpResponse } from 'msw';
-
-import { createMockStatus } from '@testing/factories/status.factory';
-
-export const handlers = [
-  http.get('/api/status', () => {
-    return HttpResponse.json(createMockStatus());
-  }),
-];
+export const handlers = [];
